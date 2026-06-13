12a59c8876b5dbd6c0c84ed32d2115a2f28d5e12
Qudix <17361645+Qudix@users.noreply.github.com>
2026-06-13 16:59:42 -0500

    feat: update

--- xmake.lua
+++ xmake.lua
@@ -1,15 +1,10 @@
--- set xmake version
-set_xmakever("2.9.4")
-
--- include local folders
+-- include subprojects
 includes("lib/commonlibsf")
 
--- set project
+-- set project constants
 set_project("BakaConsoleEditorIDs")
 set_version("3.0.0")
 set_license("GPL-3.0")
-
--- set defaults
 set_languages("c++23")
 set_warnings("allextra")
 
@@ -19,16 +14,12 @@ add_rules("plugin.vsxmake.autoupdate")
 
 -- enable lto
 set_policy("build.optimization.lto", true)
-set_policy("package.requires_lock", true)
 
 -- set config
 set_config("commonlib_xbyak", true)
 
 -- setup targets
 target("BakaConsoleEditorIDs")
-    -- bind local dependencies
-    add_deps("commonlibsf")
-
     -- add commonlibsf plugin
     add_rules("commonlibsf.plugin", {
         name = "BakaConsoleEditorIDs",
